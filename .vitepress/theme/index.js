cc9b7207bf23c9a3fa99070c770e54534aaa6871
Morgan Roderick <morgan@roderick.dk>
2026-04-03 22:37:39 +0200

    feat: configure mermaid renderer in vitepress theme

---
+++ .vitepress/theme/index.js
@@ -0,0 +1,30 @@
+import { h, nextTick, watch } from "vue";
+import DefaultTheme from "vitepress/theme";
+import { useData } from "vitepress";
+import { createMermaidRenderer } from "vitepress-mermaid-renderer";
+
+export default {
+  extends: DefaultTheme,
+  Layout: () => {
+    const { isDark } = useData();
+
+    const initMermaid = () => {
+      const _mermaidRenderer = createMermaidRenderer({
+        theme: isDark.value ? "dark" : "forest"
+      });
+    };
+
+    // initial mermaid setup
+    nextTick(() => initMermaid());
+
+    // on theme change, re-render mermaid charts
+    watch(
+      () => isDark.value,
+      () => {
+        initMermaid();
+      }
+    );
+
+    return h(DefaultTheme.Layout);
+  }
+};
